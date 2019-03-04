82a8b6f09820d991c8c074c8d9a5a9cec2714084
ted <ted10401@gmail.com>
2019-03-04 22:20:07 +0800

    Initialize the project. Implement the template feature for the new custom post-processing effect.

---
+++ Assets/PostProcessingExpansionPacks/Editor/CustomPostProcessingEndNameEditAction.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using System.IO;
+
+public class CustomPostProcessingEndNameEditAction : EndNameEditAction
+{
+    private const string TEMPLATE_PATH = "PostProcessingExpansionPacks/Editor/Templates/";
+
+    public override void Action(int instanceId, string pathName, string resourceFile)
+    {
+        string scriptName = Path.GetFileNameWithoutExtension(pathName);
+        CreateFile(pathName, scriptName, "shader", "TemplateShader.txt");
+        CreateFile(pathName, scriptName + "Renderer", "cs", "TemplateRenderer.txt");
+        CreateFile(pathName, scriptName + "Settings", "cs", "TemplateSettings.txt");
+
+        //Debug.LogErrorFormat("pathName = {0}, resourceFile = {1}", pathName, resourceFile);
+
+        //string scriptName = Path.GetFileNameWithoutExtension(pathName);
+        //string text = File.ReadAllText(resourceFile);
+
+        //text = text.Replace("$CUSTOMNAME", scriptName);
+
+        //StreamWriter sr = File.CreateText(pathName);
+        //sr.WriteLine(text);
+        //sr.Close();
+
+        //AssetDatabase.ImportAsset(pathName);
+        //ProjectWindowUtil.ShowCreatedAsset(AssetDatabase.LoadAssetAtPath<TextAsset>(pathName));
+        //AssetDatabase.Refresh();
+    }
+
+    private void CreateFile(string pathName, string fileName, string extension, string resourceFile)
+    {
+        pathName = Path.ChangeExtension(pathName, extension);
+        string scriptName = Path.GetFileNameWithoutExtension(pathName);
+        pathName = pathName.Replace(scriptName, fileName);
+
+        resourceFile = Path.Combine(Application.dataPath, TEMPLATE_PATH + resourceFile);
+        string text = File.ReadAllText(resourceFile);
+        text = text.Replace("$CUSTOMNAME", scriptName);
+
+        StreamWriter sr = File.CreateText(pathName);
+        sr.WriteLine(text);
+        sr.Close();
+
+        AssetDatabase.ImportAsset(pathName);
+        ProjectWindowUtil.ShowCreatedAsset(AssetDatabase.LoadAssetAtPath<TextAsset>(pathName));
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/PostProcessingExpansionPacks/Editor/CustomPostProcessingTool.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class CustomPostProcessingTool
+{
+    private const string MENU_ITEM_PATH = "Assets/Create/Custom Post-processing Effect";
+
+    [MenuItem(MENU_ITEM_PATH)]
+    private static void Create()
+    {
+        CreateShader();
+    }
+
+    private static void CreateShader()
+    {
+        CustomPostProcessingEndNameEditAction customPostProcessingEndNameEditAction = ScriptableObject.CreateInstance<CustomPostProcessingEndNameEditAction>();
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, customPostProcessingEndNameEditAction, "NewCustomPostProcessingShader.shader", EditorGUIUtility.IconContent("Shader Icon").image as Texture2D, string.Empty);
+    }
+}
