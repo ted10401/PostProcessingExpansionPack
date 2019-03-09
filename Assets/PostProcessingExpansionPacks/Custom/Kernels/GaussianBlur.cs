d3be40a1982a508a40508a9b0855b97353335ded
ted <ted10401@gmail.com>
2019-03-09 12:39:05 +0800

    Add gaussian blur effect.

---
+++ Assets/PostProcessingExpansionPacks/Custom/Kernels/GaussianBlur.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace UnityEngine.Rendering.PostProcessing
+{
+    [Serializable]
+    [PostProcess(typeof(GaussianBlurRenderer), PostProcessEvent.AfterStack, "Custom/Kernels/GaussianBlur")]
+    public sealed class GaussianBlur : PostProcessEffectSettings
+    {
+        
+    }
+
+    internal sealed class GaussianBlurRenderer : PostProcessEffectRenderer<GaussianBlur>
+    {
+        public override void Render(PostProcessRenderContext context)
+        {
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/GaussianBlur"));
+            
+            
+            
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+}
