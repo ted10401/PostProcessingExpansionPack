566d5afdb7be8d18b5556b4724abd6eb9fb0e995
ted <ted10401@gmail.com>
2019-03-25 22:01:35 +0800

    Fixed the blink issue in Kawase blur.

--- Assets/PostProcessingExpansionPacks/CustomPostProcessingEffects/Blur/KawaseBlur.cs
+++ Assets/PostProcessingExpansionPacks/CustomPostProcessingEffects/Blur/KawaseBlur.cs
@@ -11,11 +11,13 @@ namespace UnityEngine.Rendering.PostProcessing
 
     internal sealed class KawaseBlurRenderer : PostProcessEffectRenderer<KawaseBlur>
     {
-        private int m_downsample;
+        private int m_downsample1;
+        private int m_downsample2;
 
         public override void Init()
         {
-            m_downsample = Shader.PropertyToID("_Downsample");
+            m_downsample1 = Shader.PropertyToID("_Downsample1");
+            m_downsample2 = Shader.PropertyToID("_Downsample2");
         }
 
         public override void Render(PostProcessRenderContext context)
@@ -24,21 +26,17 @@ namespace UnityEngine.Rendering.PostProcessing
 
             var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Blurs/KawaseBlur"));
 
-            context.GetScreenSpaceTemporaryRT(context.command, m_downsample, 0, context.sourceFormat, RenderTextureReadWrite.Default, FilterMode.Bilinear, context.width, context.height);
+            context.GetScreenSpaceTemporaryRT(context.command, m_downsample1);
+            context.GetScreenSpaceTemporaryRT(context.command, m_downsample2);
 
-            var last = context.source;
+            context.command.BlitFullscreenTriangle(context.source, m_downsample1, sheet, 0);
+            context.command.BlitFullscreenTriangle(m_downsample1, m_downsample2, sheet, 1);
+            context.command.BlitFullscreenTriangle(m_downsample2, m_downsample1, sheet, 2);
+            context.command.BlitFullscreenTriangle(m_downsample1, m_downsample2, sheet, 2);
+            context.command.BlitFullscreenTriangle(m_downsample2, context.destination, sheet, 3);
 
-            context.command.BlitFullscreenTriangle(last, m_downsample, sheet, 0);
-            last = m_downsample;
-            context.command.BlitFullscreenTriangle(last, m_downsample, sheet, 1);
-            last = m_downsample;
-            context.command.BlitFullscreenTriangle(last, m_downsample, sheet, 2);
-            last = m_downsample;
-            context.command.BlitFullscreenTriangle(last, m_downsample, sheet, 2);
-            last = m_downsample;
-            context.command.BlitFullscreenTriangle(last, context.destination, sheet, 3);
-
-            context.command.ReleaseTemporaryRT(m_downsample);
+            context.command.ReleaseTemporaryRT(m_downsample1);
+            context.command.ReleaseTemporaryRT(m_downsample2);
 
             context.command.EndSample("KawaseBlur");
         }
